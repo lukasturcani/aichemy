c515a4e7976a39edbee3ad824e15e9248a979cff
Lukas Turcani <lukasturcani93@gmail.com>
2024-09-28 14:43:09 +0100

    wip

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,20 +1,5 @@
 // #![warn(rust_2018_idioms, missing_debug_implementations, missing_docs)]
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
 pub mod nmr {
 
     pub mod nomad_nmr {
