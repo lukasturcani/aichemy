8224774011c6d009f722198c236f896f4a1c93ca
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-13 19:14:27 +0100

    omg nice

--- rust/tests/nomad_nmr_download.rs
+++ rust/tests/nomad_nmr_download.rs
@@ -1,6 +1,7 @@
-use std::io::{Seek, SeekFrom, Write};
+use std::io::{Read, Seek, SeekFrom, Write};
 
 use aichemy::nmr::nomad_nmr::{AutoExperimentQuery, Client};
+use zip::ZipArchive;
 
 #[test]
 fn download_all() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,5 +14,14 @@ fn download_all() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = tempfile::tempfile()?;
     file.write_all(experiments.get()?.as_ref())?;
     file.seek(SeekFrom::Start(0))?;
+    let mut zip = ZipArchive::new(file)?;
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i)?;
+        let name = file.name();
+        println!("{name}");
+        let mut s = String::new();
+        file.read_to_string(&mut s)?;
+        println!("{s}");
+    }
     Ok(())
 }
