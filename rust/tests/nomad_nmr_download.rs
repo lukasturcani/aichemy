661666161eae1554369d4752554e8bfb47f0c827
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-13 14:40:47 +0100

    wip

--- rust/tests/nomad_nmr_download.rs
+++ rust/tests/nomad_nmr_download.rs
@@ -2,7 +2,11 @@ use aichemy::nmr::nomad_nmr::{Client, ExperimentQuery};
 
 #[test]
 fn download_all() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::login("http://localhost:8080", "admin", "foo")?;
+    let client = Client::login(
+        std::env::var("NOMAD_URL").unwrap_or("http://localhost:8080".into()),
+        "admin",
+        "foo",
+    )?;
     let experiments = client.experiments(ExperimentQuery::default())?;
     std::fs::write("/home/lt912/experiments.zip", experiments.get()?)?;
     Ok(())
