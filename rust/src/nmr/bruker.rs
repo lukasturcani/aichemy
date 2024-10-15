76288d8d75cc818929de6e6ad23847fff8053b20
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-15 10:07:03 +0100

    wip

--- rust/src/nmr/bruker.rs
+++ rust/src/nmr/bruker.rs
@@ -1,1 +1,25 @@
 //! Tools to interact with Bruker data.
+
+use std::path::{Path, PathBuf};
+
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone)]
+pub enum Error {
+    #[error("Could not find Bruker data in {0}")]
+    NotFound(PathBuf),
+}
+
+pub fn read_pdata(dir: impl AsRef<Path>) -> Result<(), Error> {
+    let data_dirs = [
+        dir.as_ref().join("1r"),
+        dir.as_ref().join("2rr"),
+        dir.as_ref().join("3rrr"),
+    ];
+    let data_dir = data_dirs
+        .iter()
+        .find(|d| d.exists())
+        .ok_or_else(|| Error::NotFound(dir.as_ref().into()))?;
+
+    Ok(())
+}
