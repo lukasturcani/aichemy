ebd2a1686d4672c99b0d9a5239a4af7ea51f6ba5
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-15 16:07:02 +0100

    wip

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,4 +5,5 @@
 //! computational data.
 #![warn(rust_2018_idioms, missing_debug_implementations, missing_docs)]
 
+pub mod io;
 pub mod nmr;
--- rust/src/nmr/bruker.rs
+++ rust/src/nmr/bruker.rs
@@ -1,3 +1,1 @@
 //! Tools to interact with Bruker data.
-
-mod jcamp;
