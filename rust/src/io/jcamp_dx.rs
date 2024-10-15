0adb2b1eeac39e64aeeef80cdf7ead00a802dabb
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-15 19:24:33 +0100

    wip

--- rust/src/io/jcamp_dx.rs
+++ rust/src/io/jcamp_dx.rs
@@ -1,1 +1,36 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{alpha1, one_of},
+    sequence::delimited,
+    IResult,
+};
 
+struct Parser {}
+
+struct DataLabel(String);
+
+struct TypedDataLabel(String);
+
+fn data_label_name(input: &str) -> IResult<&str, String> {
+    one_of(" -/\\_")
+}
+
+fn data_label(input: &str) -> IResult<&str, DataLabel> {
+    let (remaining, output) = delimited(tag("##"), data_label_name, tag("="))(input)?;
+    Ok((remaining, DataLabel(output.to_uppercase())))
+}
+
+fn typed_data_label(input: &str) -> IResult<&str, TypedDataLabel> {
+    let (remaining, output) = delimited(tag("##."), data_label_name, tag("="))(input)?;
+    Ok((remaining, TypedDataLabel(output)))
+}
+
+impl Parser {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn parse(input: &str) {
+        todo!()
+    }
+}
