a6944e6993c6f5621a6ab7b3bfff393fa4129160
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-16 11:57:08 +0100

    nice

--- rust/src/io/jcamp_dx.rs
+++ rust/src/io/jcamp_dx.rs
@@ -1,18 +1,24 @@
 use nom::{
     bytes::complete::tag,
-    character::complete::{alpha1, one_of},
-    sequence::delimited,
+    character::complete::{alphanumeric1, one_of},
+    combinator::{consumed, recognize},
+    multi::{many0, many1},
+    sequence::{delimited, terminated},
     IResult,
 };
 
-struct Parser {}
+pub struct Parser {}
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct DataLabel(String);
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct TypedDataLabel(String);
 
 fn data_label_name(input: &str) -> IResult<&str, String> {
-    one_of(" -/\\_")
+    let (remaining, (_, output)) =
+        consumed(many1(terminated(alphanumeric1, many0(one_of(" -/\\_")))))(input)?;
+    Ok((remaining, output.join("")))
 }
 
 fn data_label(input: &str) -> IResult<&str, DataLabel> {
@@ -22,7 +28,7 @@ fn data_label(input: &str) -> IResult<&str, DataLabel> {
 
 fn typed_data_label(input: &str) -> IResult<&str, TypedDataLabel> {
     let (remaining, output) = delimited(tag("##."), data_label_name, tag("="))(input)?;
-    Ok((remaining, TypedDataLabel(output)))
+    Ok((remaining, TypedDataLabel(output.into())))
 }
 
 impl Parser {
@@ -32,5 +38,17 @@ impl Parser {
 
     fn parse(input: &str) {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_data_label() {
+        let (remaining, output) = data_label("##O-B  SER\\va/TiON232_TYPE=SOLID_ANODE").unwrap();
+        assert_eq!(remaining, "SOLID_ANODE");
+        assert_eq!(output, DataLabel("OBSERVATION232TYPE".into()));
     }
 }
