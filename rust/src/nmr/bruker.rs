1e60bdf43a8bfff0dc32c36fdf28dde0b1a41117
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-23 22:39:23 +0100

    wip

--- rust/src/nmr/bruker.rs
+++ rust/src/nmr/bruker.rs
@@ -1,1 +1,41 @@
 //! Tools to interact with Bruker data.
+
+use std::{fs, path::Path};
+
+use super::io::Error;
+
+enum DataType {
+    Float64,
+    Integer32,
+}
+
+enum Endianness {
+    Little,
+    Big,
+}
+
+fn read_bruker_binary_file(
+    path: impl AsRef<Path>,
+    dtype: DataType,
+    endianness: Endianness,
+) -> Result<Vec<f64>, Error> {
+    let array = fs::read(path).map_err(|source| Error::Read { source })?;
+    match (dtype, endianness) {
+        (DataType::Float64, Endianness::Little) => Ok(array
+            .chunks_exact(8)
+            .map(|chunk| f64::from_le_bytes(chunk.try_into().unwrap()))
+            .collect()),
+        (DataType::Float64, Endianness::Big) => Ok(array
+            .chunks_exact(8)
+            .map(|chunk| f64::from_be_bytes(chunk.try_into().unwrap()))
+            .collect()),
+        (DataType::Integer32, Endianness::Little) => Ok(array
+            .chunks_exact(4)
+            .map(|chunk| i32::from_le_bytes(chunk.try_into().unwrap()) as f64)
+            .collect()),
+        (DataType::Integer32, Endianness::Big) => Ok(array
+            .chunks_exact(4)
+            .map(|chunk| i32::from_be_bytes(chunk.try_into().unwrap()) as f64)
+            .collect()),
+    }
+}
--- rust/src/nmr/io.rs
+++ rust/src/nmr/io.rs
@@ -1,13 +1,23 @@
 //! Tools for NMR I/O, such a reading and writing files.
 
+use std::io;
+
 use thiserror::Error;
 
 pub mod jcamp_dx;
 
 /// Error which may occur when doing NMR I/O.
-#[derive(Debug, Error, PartialEq)]
+#[derive(Debug, Error)]
 pub enum Error {
     /// Failed to parse a file.
     #[error("Failed to parse file: {0}")]
     Parse(String),
+
+    /// Failed to read a file.
+    #[error("Failed to read file: {source}")]
+    Read { source: io::Error },
+
+    /// Failed to write a file.
+    #[error("Failed to write file: {0}")]
+    Write(String),
 }
