2a3bc885cdf2b0709577b1ecf0d6528ec981c000
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-24 13:52:56 +0100

    wip

--- rust/src/nmr/bruker.rs
+++ rust/src/nmr/bruker.rs
@@ -1,32 +1,38 @@
 //! Tools to interact with Bruker data.
 
-use std::{fs, path::Path};
+use std::{collections::HashMap, fs, path::Path};
 
 use crate::Error;
 
-use super::io::jcamp_dx;
+use super::io::jcamp_dx::Value;
 
-enum DataType {
+pub enum DataType {
     Float64,
     Integer32,
 }
 
-enum Endianness {
+pub enum Endianness {
     Little,
     Big,
 }
 
-fn read_bruker_binary_file(
-    path: impl AsRef<Path>,
-    dtype: DataType,
+/// Interpret the content of a Bruker binary file.
+///
+/// # Examples
+/// ```no_run
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// use aichemy::nmr::bruker{self, Procs};
+/// use aichemy::nmr::io::jcamp_dx;
+/// use std::fs;
+/// let procs = Procs(jcamp_dx::parse(fs::read("procs")?)?);
+/// let spectrum = bruker::read_binary(fs::read("1r")?, procs.data_type()?, procs.endianness()?)?;
+/// ```
+pub fn read_binary(
+    array: Vec<u8>,
+    data_type: DataType,
     endianness: Endianness,
 ) -> Result<Vec<f64>, Error> {
-    let path = path.as_ref();
-    let array = fs::read(path).map_err(|source| Error::Io {
-        message: format!("failed to read {path:?}"),
-        source: Some(source.into()),
-    })?;
-    match (dtype, endianness) {
+    match (data_type, endianness) {
         (DataType::Float64, Endianness::Little) => Ok(array
             .chunks_exact(8)
             .map(|chunk| f64::from_le_bytes(chunk.try_into().unwrap()))
@@ -50,68 +56,61 @@ fn scale_data(data: &mut [f64]) {
     todo!()
 }
 
-fn read_1d_spectrum(
-    binary: impl AsRef<Path>,
-    procs: impl AsRef<Path>,
-    acqus: impl AsRef<Path>,
-) -> Result<(), Error> {
-    let binary = binary.as_ref();
-    let procs = procs.as_ref();
-    let acqus = acqus.as_ref();
-    let procs = jcamp_dx::parse(fs::read(procs).map_err(|source| Error::Io {
-        message: format!("failed to read {procs:?}"),
-        source: Some(source.into()),
-    })?)?;
-    let acqus = jcamp_dx::parse(fs::read(acqus).map_err(|source| Error::Io {
-        source: Some(source.into()),
-        message: format!("failed to read {acqus:?}"),
-    })?)?;
-    let si = procs
-        .get("$SI")
-        .ok_or(Error::NmrError {
-            message: format!("$SI variable missing from {procs:?}"),
-        })?
-        .as_integer()
-        .ok_or(Error::NmrError {
-            message: format!("$SI variable is not an integer in {procs:?}"),
-        })?;
-    let xdim = procs
-        .get("$XDIM")
-        .ok_or(Error::NmrError {
-            message: format!("$XDIM variable missing from {procs:?}"),
-        })?
-        .as_integer()
-        .ok_or(Error::NmrError {
-            message: format!("$XDIM variable is not an integer in {procs:?}"),
-        })?;
+fn read_2d_spectrum(binary: impl AsRef<Path>, procs: impl AsRef<Path>, acqus: impl AsRef<Path>) {
+    // let si = procs
+    //     .get("$SI")
+    //     .ok_or(Error::NmrError {
+    //         message: format!("$SI variable missing from {procs:?}"),
+    //     })?
+    //     .as_integer()
+    //     .ok_or(Error::NmrError {
+    //         message: format!("$SI variable is not an integer in {procs:?}"),
+    //     })?;
+    // let xdim = procs
+    //     .get("$XDIM")
+    //     .ok_or(Error::NmrError {
+    //         message: format!("$XDIM variable missing from {procs:?}"),
+    //     })?
+    //     .as_integer()
+    //     .ok_or(Error::NmrError {
+    //         message: format!("$XDIM variable is not an integer in {procs:?}"),
+    //     })?;
+    todo!();
+}
 
-    let endianness = match procs.get("$BYTEORD") {
-        None => Endianness::Little,
-        Some(byte_order) => {
-            let byte_order = byte_order.as_integer().ok_or(Error::NmrError {
-                message: format!("$BYTEORD variable is not an integer in {procs:?}"),
-            })?;
-            if byte_order == 1 {
-                Endianness::Big
-            } else {
-                Endianness::Little
+pub struct Procs(pub HashMap<String, Value>);
+
+impl Procs {
+    pub fn endianness(&self) -> Result<Endianness, Error> {
+        match self.0.get("$BYTEORD") {
+            None => Ok(Endianness::Little),
+            Some(byte_order) => {
+                let byte_order = byte_order.as_integer().ok_or(Error::NmrError {
+                    message: format!("$BYTEORD variable is not an integer: {byte_order:?}"),
+                })?;
+                if byte_order == 1 {
+                    Ok(Endianness::Big)
+                } else {
+                    Ok(Endianness::Little)
+                }
             }
         }
-    };
-    let data_type = match procs.get("$DTYPP") {
-        None => DataType::Integer32,
-        Some(dtype) => {
-            let dtype = dtype.as_integer().ok_or(Error::NmrError {
-                message: format!("$DTYPP variable is not an integer in {procs:?}"),
-            })?;
-            if dtype == 2 {
-                DataType::Float64
-            } else {
-                DataType::Integer32
+    }
+
+    pub fn data_type(&self) -> Result<DataType, Error> {
+        let dtype = self.0.get("$DTYPP");
+        Ok(match dtype {
+            None => DataType::Integer32,
+            Some(dtype) => {
+                let dtype = dtype.as_integer().ok_or(Error::NmrError {
+                    message: format!("$DTYPP variable is not an integer: {dtype:?}"),
+                })?;
+                if dtype == 2 {
+                    DataType::Float64
+                } else {
+                    DataType::Integer32
+                }
             }
-        }
-    };
-    let mut data = read_bruker_binary_file(binary, data_type, endianness)?;
-    scale_data(&mut data);
-    Ok(())
+        })
+    }
 }
