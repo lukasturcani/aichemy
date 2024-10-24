f0257ba4871e1875db2b526633325be08296bd8e
Lukas Turcani <lukasturcani93@gmail.com>
2024-10-24 16:06:54 +0100

    Add example

--- rust/examples/plot_1d_nmr_spectrum.rs
+++ rust/examples/plot_1d_nmr_spectrum.rs
@@ -1,6 +1,14 @@
 use aichemy::nmr::bruker::{self, Procs};
 use aichemy::nmr::io::jcamp_dx;
+use clap::Parser;
 use std::fs;
+use std::path::{Path, PathBuf};
+
+#[derive(Parser)]
+struct Cli {
+    /// Path to a Bruker NMR spectrum directory.
+    nmr_directory: PathBuf,
+}
 
 fn main() {
     if let Err(error) = run() {
@@ -10,9 +18,38 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let procs = Procs(jcamp_dx::parse(fs::read("procs")?)?);
-    let mut spectrum =
-        bruker::read_binary(fs::read("1r")?, procs.data_type()?, procs.endianness()?)?;
+    let cli = Cli::parse();
+    let procs = read_procs(&cli.nmr_directory)?;
+    let data = read_spectrum(&cli.nmr_directory)?;
+    let mut spectrum = bruker::read_binary(data, procs.data_type()?, procs.endianness()?)?;
     bruker::scale(&mut spectrum, procs.scale()?);
     Ok(())
+}
+
+fn read_procs(nmr_directory: impl AsRef<Path>) -> Result<Procs, Box<dyn std::error::Error>> {
+    let procs_path = glob::glob(
+        nmr_directory
+            .as_ref()
+            .join("pdata/*/procs")
+            .to_str()
+            .ok_or(anyhow::anyhow!("invalid path"))?,
+    )?
+    .next()
+    .ok_or(anyhow::anyhow!("no procs file found"))??;
+
+    Ok(Procs(jcamp_dx::parse(fs::read(procs_path)?)?))
+}
+
+fn read_spectrum(nmr_directory: impl AsRef<Path>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let spectrum_path = glob::glob(
+        nmr_directory
+            .as_ref()
+            .join("pdata/*/1r")
+            .to_str()
+            .ok_or(anyhow::anyhow!("invalid path"))?,
+    )?
+    .next()
+    .ok_or(anyhow::anyhow!("no spectrum file found"))??;
+
+    Ok(fs::read(spectrum_path)?)
 }
